aa98e492b68a92f6334c4da225f0fd1b7529caad
MiriamPecora <miriampecora97@gmail.com>
2024-07-10 18:31:17 +0200

    [ADD] Created controller for trains page and created trains table using migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Guest\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [PageController::class, 'index'])->name('homepage');
+
+Route::get('/trains', [TrainController::class, 'index'])->name('trains');
